45d8a3b923a16a13eda7b62e09a65c022bcee6fa
RobinsonMosqueraCubides <152357795+RobinsonMosqueraCubides@users.noreply.github.com>
2024-01-18 21:59:16 -0500

    Se agrega el boton interactivo, aparece un error con la posiscion del 'section' la cual se sobrepone al 'nav'

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 // AOS.init({
 //     offset:1
 // });
+let links = document.querySelector('.links')
+
+document.querySelector('.boton').addEventListener('click', function(){
+    links.classList.toggle('active')
+})
