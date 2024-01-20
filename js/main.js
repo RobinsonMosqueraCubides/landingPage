8f42c2ac18b96f722e0316a816403d8d7e492585
RobinsonMosqueraCubides <152357795+RobinsonMosqueraCubides@users.noreply.github.com>
2024-01-19 21:56:28 -0500

    Se realiza los ultimos comentarios

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // AOS.init({
 //     offset:1
 // });
+
+//Se le indica al programa que cuando la pantalla sea de un celular y se le de click al boton del menu cambie al estado activo
 let links = document.querySelector('.links')
 
 document.querySelector('.boton').addEventListener('click', function(){
